1c7404e09aa162d1dff20ac2f354677fa38ae27b
13Hz <13Hz@users.noreply.github.com>
2023-06-01 10:38:59 +0000

    Code style fix

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -15,6 +15,7 @@ if (!function_exists('get_sqlite_db_path')) {
 if (!function_exists('declOfNum')) {
     /**
      * Функция склонения числительных в русском языке
+     *
      * @param int $number Число которое нужно просклонять
      * @param array $titles Массив слов для склонения ['%d комментарий', '%d комментария', '%d комментариев']
      * @return string
@@ -23,6 +24,7 @@ if (!function_exists('declOfNum')) {
     {
         $cases = [2, 0, 1, 1, 1, 2];
         $format = $titles[($number % 100 > 4 && $number % 100 < 20) ? 2 : $cases[min($number % 10, 5)]];
+
         return sprintf($format, $number);
     }
 }
